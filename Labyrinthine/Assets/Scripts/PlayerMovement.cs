4e6600250bf55be9bbf19a59d49ffd74de67e31e
Umbra-Ignis <devon.j.sanders@gmail.com>
2017-09-11 16:33:37 +1000

    Added Colors and Movement Script

---
+++ Labyrinthine/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float ForwardMovementSpeed = 0.2f, DifferenceInZBetweenPlatforms = 2.0f;
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        transform.Translate(new Vector3(-ForwardMovementSpeed, 0.0f, 0.0f));
+        //Checks if the left arrow key is pressed
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position -= new Vector3(0.0f, 0.0f, DifferenceInZBetweenPlatforms);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(0.0f, 0.0f, DifferenceInZBetweenPlatforms);
+        }
+	}
+}
