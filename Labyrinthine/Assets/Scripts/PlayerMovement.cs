626a37997a3dd12de0cd7b2ae230fe586d797da3
Umbra-Ignis <devon.j.sanders@gmail.com>
2017-09-18 15:25:02 +1000

    Updated concept and new level elements

--- Labyrinthine/Assets/Scripts/PlayerMovement.cs
+++ Labyrinthine/Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float ForwardMovementSpeed = 0.2f, DifferenceInZBetweenPlatforms = 2.0f;
+    public float ForwardMovementSpeed = 0.2f, DifferenceInXBetweenPlatforms = 3.4f;
+    /// <summary>
+    /// Storage for the distance in movement
+    /// </summary>
+    Vector3 movement;
 	// Update is called once per frame
+    void Start()
+    {
+        movement = new Vector3(DifferenceInXBetweenPlatforms, 0.0f, 0.0f);
+    }
+    void Update()
+    {
+        if(Debug.isDebugBuild)
+        {
+            movement = new Vector3(DifferenceInXBetweenPlatforms, 0.0f, 0.0f);
+            Debug.Log("IN DEBUG MODE");
+        }
+        //Left Movement
+        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = transform.position - movement;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            transform.position = transform.position + movement;
+        }
+        //Right Movement
+        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = transform.position + movement;
+        }
+        if(Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            transform.position = transform.position - movement;
+        }
+    }
 	void FixedUpdate ()
     {
-        transform.Translate(new Vector3(-ForwardMovementSpeed, 0.0f, 0.0f));
-        //Checks if the left arrow key is pressed
-		if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.position -= new Vector3(0.0f, 0.0f, DifferenceInZBetweenPlatforms);
-        }
-        if(Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(0.0f, 0.0f, DifferenceInZBetweenPlatforms);
-        }
+        transform.Translate(Vector3.forward * ForwardMovementSpeed);
 	}
 }
