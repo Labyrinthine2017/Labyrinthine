7afbe5dab3e6db5d4c53e73a81bc4fdd0f1d82ef
Rexman9023 <kingmabrent@gmail.com>
2017-10-02 12:17:45 +1100

    Modified GameManager.cs and Note.cs, impliminting the code for combo scores.

--- Labyrinthine/Assets/Scripts/GameManager.cs
+++ Labyrinthine/Assets/Scripts/GameManager.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    public int combo2Score = 5, combo3Score = 12, combo4Score = 19;
+    float gameScore { get; set; }
+    public int comboScore { get; set; }
+    int combo { get; set; }
 	// Use this for initialization
 	void Start ()
     {
         Application.targetFrameRate = 60;
 	}
+
+    void FixedUpdate()
+    {
+        if(comboScore >= combo2Score)
+        {
+            combo = 2;
+        }
+        if(comboScore >= combo3Score)
+        {
+            combo = 3;
+        }
+        if (comboScore >= combo4Score)
+        {
+            combo = 4;
+        }
+    }
+    public void AddScore(float a_score)
+    {
+        gameScore += a_score * combo;
+    }
+    public void SubtractScore(float a_score)
+    {
+        gameScore -= a_score;
+    }
+    public void ResetCombo()
+    {
+        combo = 1;
+        comboScore = 0;
+    }
 
 }
--- Labyrinthine/Assets/Scripts/Notes/Note.cs
+++ Labyrinthine/Assets/Scripts/Notes/Note.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 public class Note : MonoBehaviour
 {
     public float fuelEffectAmount = 1.0f;
+    GameManager manager;
+    GameObject player;
 
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        manager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+    }
+    void FixedUpdate()
+    {
+        if(player.transform.position.z > transform.position.z)
+        {
+            manager.ResetCombo();
+        }
+    }
 
     void OnTriggerStay(Collider col)
     {
@@ -14,6 +28,7 @@ public class Note : MonoBehaviour
             if (Input.GetKey(KeyCode.Space))
             {
                 col.GetComponent<EngineBehaviour>().CoolEngineByAmount(fuelEffectAmount);
+                manager.comboScore += 1;
                 Destroy(this.gameObject);
             }
 
