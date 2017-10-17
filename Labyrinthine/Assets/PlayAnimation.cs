7988fdb27b314fc82aff618e92232f6fef05fc4f
Rexman9023 <kingmabrent@gmail.com>
2017-10-18 10:13:19 +1100

    Upload the animations and the required scripts

---
+++ Labyrinthine/Assets/PlayAnimation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour
+{
+    public Animator anim;
+    private bool crossed = false;
+    private bool canLaser = true;
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyUp(KeyCode.L))
+        {
+            if (canLaser == true)
+            {
+                if (crossed == false)
+                {
+                    anim.SetTrigger("CrossLasers");
+                    crossed = true;
+                }
+                else
+                {
+                    anim.SetTrigger("UncrossLasers");
+                    crossed = false;
+                }
+
+                canLaser = false;
+            }
+        }
+	}
+
+    public void HideLaser()
+    {
+        //turn off particles
+        //turn off collisions
+        canLaser = true;
+    }
+}
