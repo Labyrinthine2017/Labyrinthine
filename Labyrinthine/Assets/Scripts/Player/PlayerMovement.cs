eaf19f6e3d1816d6990cfa245db5c27194cd7f1b
Rexman9023 <kingmabrent@gmail.com>
2017-10-23 16:37:32 +1100

    Updated movement script to allow for magnetization and non magnetization movement

--- Labyrinthine/Assets/Scripts/Player/PlayerMovement.cs
+++ Labyrinthine/Assets/Scripts/Player/PlayerMovement.cs
@@ -16,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     Vector3 leftLanePos, rightLanePos, centreLanePos;
     Vector3 playerOrignalPosition;
     bool movingLeft = false, movingRight = false, hovering = false;
+    bool inLeftLane = false, inMidLane = true, inRightLane = false;
+    [SerializeField] bool magnatise = false;
 
     //For Xbox360 controller
     public XboxController controller { get; set; }
@@ -33,120 +35,194 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
-		if (controller != XboxController.All)
-		//if (true)
+        if (magnatise)
         {
-            if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)))
+            if (controller != XboxController.All)
+            //if (true)
             {
-                movingLeft = false;
-                movingRight = false;
+                if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)))
+                {
+                    movingLeft = false;
+                    movingRight = false;
+                }
+                else
+                {
+                    //Left Movement
+                    if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+                    {
+                        movingLeft = true;
+                    }
+                    else
+                    {
+                        movingLeft = false;
+                    }
+                    //Right Movement
+                    if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+                    {
+                        movingRight = true;
+                    }
+                    else
+                    {
+                        movingRight = false;
+                    }
+                }
+                if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+                {
+                    hovering = true;
+                }
+                else
+                {
+                    hovering = false;
+                }
             }
             else
             {
-                //Left Movement
-                if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+                if (XCI.GetAxisRaw(XboxAxis.LeftStickX, controller) < 0.0f)
                 {
                     movingLeft = true;
+                    movingRight = false;
                 }
-                else
-                {
-                    movingLeft = false;
-                }
-                //Right Movement
-                if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+                if (XCI.GetAxisRaw(XboxAxis.LeftStickX, controller) > 0.0f)
                 {
                     movingRight = true;
+                    movingLeft = false;
                 }
-                else
+                if (XCI.GetAxisRaw(XboxAxis.LeftStickX, controller) == 0.0f)
                 {
+                    movingLeft = false;
                     movingRight = false;
                 }
             }
-            if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-            {
-                hovering = true;
-            }
-            else
-            {
-                hovering = false;
-            }
-        }
-        else
+        }     
+        else if(!magnatise)
         {
-            if (XCI.GetAxisRaw(XboxAxis.LeftStickX, controller) < 0.0f)
+            if(transform.position.x == leftLanePos.x)
+            {
+                if(movingLeft)
+                {
+                    movingLeft = false;
+                }
+                inLeftLane = true;
+                inMidLane = false;
+                inRightLane = false;
+            }
+            if (transform.position.x == rightLanePos.x)
+            {
+                if(movingRight)
+                {
+                    movingRight = false;
+                }
+                inLeftLane = false;
+                inMidLane = false;
+                inRightLane = true;
+            }
+            if (transform.position.x == centreLanePos.x)
+            {
+                inLeftLane = false;
+                inMidLane = true;
+                inRightLane = false;
+            }
+
+            if((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && (!inLeftLane || !inMidLane))
             {
                 movingLeft = true;
-                movingRight = false;
             }
-            if (XCI.GetAxisRaw(XboxAxis.LeftStickX, controller) > 0.0f)
+            if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && (!inMidLane || !inRightLane))
             {
                 movingRight = true;
-                movingLeft = false;
-            }
-            if (XCI.GetAxisRaw(XboxAxis.LeftStickX, controller) == 0.0f)
-            {
-                movingLeft = false;
-                movingRight = false;
             }
         }
-        
-        
     }
 	void FixedUpdate ()
     {
         //Constant forward movement
         transform.Translate(Vector3.forward * ForwardMovementSpeed * Time.fixedDeltaTime);
-
         //Leftwards movement
         if (movingLeft)
         {
-            if (transform.position.x > leftLanePos.x)
+            if (inMidLane && !inLeftLane)
             {
-                transform.Translate(Vector3.left * LeftRightMovementSpeed * Time.fixedDeltaTime);
-                if (transform.position.x < leftLanePos.x)
+                if (transform.position.x > leftLanePos.x)
                 {
-                    transform.position = new Vector3(leftLanePos.x, transform.position.y, transform.position.z);
+                    transform.Translate(Vector3.left * LeftRightMovementSpeed * Time.fixedDeltaTime);
+                    if (transform.position.x < leftLanePos.x)
+                    {
+                        transform.position = new Vector3(leftLanePos.x, transform.position.y, transform.position.z);
+                        movingLeft = false;
+                    }
+                }
+            }
+            if(inRightLane && !inMidLane)
+            {
+                if (transform.position.x > centreLanePos.x)
+                {
+                    transform.Translate(Vector3.left * LeftRightMovementSpeed * Time.fixedDeltaTime);
+                    if (transform.position.x < centreLanePos.x)
+                    {
+                        transform.position = new Vector3(centreLanePos.x, transform.position.y, transform.position.z);
+                        movingLeft = false;
+                    }
                 }
             }
         }
         //Rightwards movement
         if (movingRight)
         {
-            if (transform.position.x < rightLanePos.x)
+            if (inMidLane && !inRightLane)
             {
-                transform.Translate(Vector3.right * LeftRightMovementSpeed * Time.fixedDeltaTime);
-                if (transform.position.x > rightLanePos.x)
+                if (transform.position.x < rightLanePos.x)
                 {
-                    transform.position = new Vector3(rightLanePos.x, transform.position.y, transform.position.z);
+                    transform.Translate(Vector3.right * LeftRightMovementSpeed * Time.fixedDeltaTime);
+                    if (transform.position.x > rightLanePos.x)
+                    {
+                        transform.position = new Vector3(rightLanePos.x, transform.position.y, transform.position.z);
+                        movingRight = false;
+                    }
                 }
             }
-        }
-        //Returns to original position
-        if (!movingRight && !movingLeft)
-        {
-            //Moves from left to center
-            if (transform.position.x < centreLanePos.x)
+            if(inLeftLane && !inMidLane)
             {
-                transform.Translate(Vector3.right * LeftRightMovementSpeed * Time.fixedDeltaTime);
-                movingLeft = false;
-                movingRight = false;
-                if (transform.position.x > centreLanePos.x)
-                {
-                    transform.position = new Vector3(centreLanePos.x, transform.position.y, transform.position.z);
-                }
-            }
-            //Moves from right to center
-            if (transform.position.x > centreLanePos.x)
-            {
-                transform.Translate(Vector3.left * LeftRightMovementSpeed * Time.fixedDeltaTime);
-                movingLeft = false;
-                movingRight = false;
                 if (transform.position.x < centreLanePos.x)
                 {
-                    transform.position = new Vector3(centreLanePos.x, transform.position.y, transform.position.z);
+                    transform.Translate(Vector3.right * LeftRightMovementSpeed * Time.fixedDeltaTime);
+                    if (transform.position.x > centreLanePos.x)
+                    {
+                        transform.position = new Vector3(centreLanePos.x, transform.position.y, transform.position.z);
+                        movingRight = false;
+                    }
                 }
             }
         }
+        if (magnatise)
+        {
+            //Returns to original position
+            if (!movingRight && !movingLeft)
+            {
+                //Moves from left to center
+                if (transform.position.x < centreLanePos.x)
+                {
+                    transform.Translate(Vector3.right * LeftRightMovementSpeed * Time.fixedDeltaTime);
+                    movingLeft = false;
+                    movingRight = false;
+                    if (transform.position.x > centreLanePos.x)
+                    {
+                        transform.position = new Vector3(centreLanePos.x, transform.position.y, transform.position.z);
+                    }
+                }
+                //Moves from right to center
+                if (transform.position.x > centreLanePos.x)
+                {
+                    transform.Translate(Vector3.left * LeftRightMovementSpeed * Time.fixedDeltaTime);
+                    movingLeft = false;
+                    movingRight = false;
+                    if (transform.position.x < centreLanePos.x)
+                    {
+                        transform.position = new Vector3(centreLanePos.x, transform.position.y, transform.position.z);
+                    }
+                }
+            }
+        }
+
         //Hovering
         if(hovering)
         {
