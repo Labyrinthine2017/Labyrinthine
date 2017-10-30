284828747c2e11da085768427dd8ee6fcc8574aa
Markus <marksturtz62@gmail.com>
2017-10-30 13:40:29 +1100

    Mark's Changes to team Lab Test NO.2
    FINISHED WORK
    
    * The game menu now has 3 available buttons (Start, Options, Exit) 
    * Exit will only work in an executable file (.exe file)
    * NoteList Bug fixes (999+ errors in noteList are now gone)
    * NoteSound Bug fixes (no selected AudioSource and is not being used anyway)
    * Created new scenes (Menu, OptionMenu, In-GameMenu/canvas) 
    
    CURRENTLY WORKING ON
    
    * Options in the MainMenu is under construction and does not work properly
    * In-game UI is under construction (using input button 'Escape' in-game)
    
    GENERIC CHANGES
    
    * Scene changes have been made for bug fixes
    * Organized folders have been made for scenes that have been created

---
+++ Labyrinthine/Assets/Scripts/UI/InGameOption.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameOption : MonoBehaviour
+{
+    private Image img;
+
+    public void Awake()
+    {
+        img = GameObject.Find("GameUI").GetComponent<Image>();
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+           if(gameObject.tag == "InGameOption")
+            {
+                img.color = Color.blue;
+            }
+        }
+	}
+}
---
+++ Labyrinthine/Assets/Scripts/UI/OptionsUI/MusicVolume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MusicVolume : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public Slider sliderVolume;
+
+    public void ControlVolume()
+    {
+        sliderVolume.value = audioSource.volume;
+    }
+}
