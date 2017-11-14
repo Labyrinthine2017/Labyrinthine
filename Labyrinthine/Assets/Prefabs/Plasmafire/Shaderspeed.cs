0063c3340aa5a92311f2242b104eeb85d27b4e99
Umbra-Ignis <devon.j.sanders@gmail.com>
2017-11-14 11:22:26 +1100

    Finished Building Road

---
+++ Labyrinthine/Assets/Prefabs/Plasmafire/Shaderspeed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaderspeed : MonoBehaviour {
+
+	Renderer rend;
+	float value = 0.0000001f;
+	public float increaseAmount;
+	// Use this for initialization
+	void Start ()
+	{
+		rend = this.GetComponent<Renderer>();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(value >= 10.0f)
+		{
+			increaseAmount = -1*increaseAmount;
+		}
+		if(value <= 0.0f)
+		{
+			increaseAmount = -1 * increaseAmount;
+		}
+		value += increaseAmount;
+
+		rend.material.SetFloat("_texturemove", value);
+	}
+}
