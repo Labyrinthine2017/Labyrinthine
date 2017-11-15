eeffb85b8af503605b59249f56b3fbcde5e32ceb
JakeReason <Jake.raymond.reason@gmail.com>
2017-11-15 11:53:35 +1100

    Fissures replaced and remade

---
+++ Labyrinthine/Assets/Fissure/Shaderspeed2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaderspeed2 : MonoBehaviour
+{
+
+	Renderer rend;
+	float material1 = 0.0000001f;
+    float material2 = 9.000009f;
+    public float increaseAmount;
+    float material1Amount;
+    float material2Amount;
+	// Use this for initialization
+	void Start ()
+	{
+		rend = this.GetComponent<Renderer>();
+        material1Amount = increaseAmount;
+        material2Amount = -increaseAmount;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(material1 >= 10.0f)
+		{
+            material1Amount = -1* material1Amount;
+		}
+		if(material1 <= 0.0f)
+		{
+            material1Amount = -1 * material1Amount;
+		}
+        material1 += material1Amount;
+		rend.material.SetFloat("_Texturemove2", material1);
+
+
+        if (material2 >= 10.0f)
+        {
+            material2Amount = -1 * material2Amount;
+        }
+        if (material2 <= 0.0f)
+        {
+            material2Amount = -1 * material2Amount;
+        }
+        material2 += material2Amount;
+        rend.material.SetFloat("_Texturemove1", material2);
+
+
+    }
+}
