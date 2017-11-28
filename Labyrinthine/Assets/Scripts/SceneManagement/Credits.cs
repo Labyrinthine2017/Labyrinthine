643d71c8955cb672235ef5368348684590736636
Markus <marksturtz62@gmail.com>
2017-11-28 14:53:53 +1100

    CHANGES MADE   *credits finished   *resized scale
    CHANGES THAT NEEDS TO BE DONE
      *god mode

---
+++ Labyrinthine/Assets/Scripts/SceneManagement/Credits.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using SceneManagement
+
+
+public class Credits : MonoBehaviour
+{
+    public void CreditScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
