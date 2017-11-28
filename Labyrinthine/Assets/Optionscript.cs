04060586dba9895225ad7de5cb007fb0af8de2b1
Markus <marksturtz62@gmail.com>
2017-11-29 09:20:01 +1100

    Adding options script to new gold

---
+++ Labyrinthine/Assets/Optionscript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Optionscript : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OptionPrompt()
+    {
+        gameObject.SetActive(!gameObject.activeInHierarchy);
+    }
+}
